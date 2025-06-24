1733f56b3f11a71ab053357c7c8ea346b3449f9d
dungstergen <nguyentiendung01062005@gmail.com>
2025-06-24 16:38:30 +0700

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// You can add your JavaScript code here. For demo, we'll log a message.
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Page loaded!');
+});
